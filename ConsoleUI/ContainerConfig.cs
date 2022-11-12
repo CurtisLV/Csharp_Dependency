57cd48ce304b2a59136ae1e2c71b6f392323467a
CurtisLV <curtislv@gmail.com>
2022-11-12 18:04:01 +0200

    Container config 2

--- ConsoleUI/ContainerConfig.cs
+++ ConsoleUI/ContainerConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autofac;
+using DemoLibrary;
 
 namespace ConsoleUI
 {
@@ -11,7 +12,14 @@ namespace ConsoleUI
     {
         public static IContainer Configure()
         {
-            //
+            var builder = new ContainerBuilder();
+
+            // registering the class
+            // whenever one is looking for IBusinessLogic, BusinessLogic will be returned
+            // respond with BusinessLogic whenever anyone needs IBusinessLogic item (new IBusinessLogic)
+            builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+
+            return builder.Build();
         }
     }
 }
