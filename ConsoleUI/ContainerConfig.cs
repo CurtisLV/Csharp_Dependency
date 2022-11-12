3cde71f670cad77cd87775a1d9915af74d3f9cc5
CurtisLV <curtislv@gmail.com>
2022-11-12 18:00:39 +0200

    Container config 1

---
+++ ConsoleUI/ContainerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace ConsoleUI
+{
+    public class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            //
+        }
+    }
+}
